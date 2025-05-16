009166d630feb2c47f2172ee406537f1d4a21e02
j3onghoon <park.j3onghoon@gmail.com>
2025-05-17 01:38:41 +0900

    viewset 구현

--- j3onghoon/blog/views.py
+++ j3onghoon/blog/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from rest_framework import viewsets, serializers
+from rest_framework.pagination import PageNumberPagination
 
-# Create your views here.
+from .models import User, Post, Comment, Attachment, Category
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.prefetch_related("attachments")
+    serializer_class = UserSerializer
+
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.prefetch_related("attachments").select_related("author", "category")
+    serializer_class = PostSerializer
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.select_related("post", "author", "parent")
+    serializer_class = CommentSerializer
