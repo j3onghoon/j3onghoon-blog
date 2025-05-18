822116d125280086f5529ce0e3fcfcda6fa42e7e
j3onghoon <park.j3onghoon@gmail.com>
2025-05-19 00:59:07 +0900

    htmx를 사용을 위한 중복 코드 추출

--- j3onghoon/blog/views.py
+++ j3onghoon/blog/views.py
@@ -6,10 +6,21 @@ from .models import User, Post, Comment, Attachment, Category
 class HomeView(TemplateView):
     template_name = "home.html"
 
+
+class HTMXMixin:
+    def get_template_names(self):
+        if self.request.headers.get("HX-Request"):
+            htmx_template = getattr(self, "htmx_template_name", None)
+            if htmx_template:
+                return [htmx_template]
+        return super().get_template_names()
+
+
 class PostListView(ListView):
     model = Post
+    template_name = "post_list.html"
+    htmx_template_name= "post_list_partial.html"
     paginate_by = 10
 
 
-class PostDetailView(DetailView):
-    model = Post
+
